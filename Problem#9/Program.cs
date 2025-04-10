497fe8b09e5e3b12dbf1554ec18f015c9d4bab18
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 08:53:20 +0500

    Palindrome Number

---
+++ Problem#9/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if(x < 0)
+        {
+            return false;
+        }
+
+        string s = x.ToString();
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        while(left < right)
+        {
+            if(s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
