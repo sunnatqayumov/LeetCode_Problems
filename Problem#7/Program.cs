46ae65a6a68f7b7152a7248c2b71e14a8caa6cd6
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 08:50:22 +0500

    Reverse Integer

---
+++ Problem#7/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution 
+{
+    public int Reverse(int x) 
+    {
+        int rev = 0;
+
+        while (x != 0) 
+        {
+            int d = x % 10;
+            x /= 10;
+
+            if (rev > 214748364 || (rev == 214748364 && d > 7)) return 0;
+            if (rev < -214748364 || (rev == -214748364 && d < -8)) return 0;
+
+            rev = rev * 10 + d;
+        }
+        return rev;
+    }
+}
