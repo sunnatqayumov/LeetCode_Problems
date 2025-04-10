1e50956195ee04096f21a0fb52556f5750c6514c
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:00:06 +0500

    Roman To Integer

---
+++ Problem#13/Program.cs
@@ -0,0 +1,60 @@
+﻿public class Solution
+{
+    public int RomanToInt(string str)
+    {
+        int total = 0;
+        int result = 0;
+
+        for(int i = str.Length - 1; i >= 0; i--)
+        {
+            char currentChar = str[i];
+            int currentValue = 0;
+
+            switch(currentChar)
+            {
+                case 'I':
+                    currentValue = 1;
+                break;
+
+                case 'V':
+                    currentValue = 5;
+                break;
+
+                case 'X':
+                    currentValue = 10;
+                break;
+
+                case 'L':
+                    currentValue = 50;
+                break;
+
+                case 'C':
+                    currentValue = 100;
+                break;
+
+                case 'D':
+                    currentValue = 500;
+                break;
+
+                case 'M':
+                    currentValue = 1000;
+                break;
+
+                default:
+                    currentValue = 0;
+                break;
+            }
+
+            if(currentValue < result)
+            {
+                total -= currentValue;
+            }
+            else
+            {
+                total += currentValue;
+            }
+            result = currentValue;
+        }
+        return total;
+    }
+}
