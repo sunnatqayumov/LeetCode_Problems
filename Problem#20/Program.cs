802da7cac2e9a3090abcc4f46e1804ccba445745
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:06:02 +0500

    Valid Parentheses

---
+++ Problem#20/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+ {
+    public bool IsValid(string s) 
+    {
+        char[] stack = new char[s.Length];  
+        int index = 0;
+
+        foreach (char c in s) 
+        {
+            if (c == '(' || c == '{' || c == '[') 
+            {
+                stack[index++] = c;  
+            } 
+            else
+            {
+                if (index == 0) 
+                    return false;  
+
+                char top = stack[--index];  
+                
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return index == 0;  
+    }
+}
