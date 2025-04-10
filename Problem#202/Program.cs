7b26f87fa6157d79753f0b2d168619cf0fa101aa
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:18:53 +0500

    Happy Number

---
+++ Problem#202/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution 
+{
+    public bool IsHappy(int n) 
+    {
+        List<int> list = new List<int>();
+        int sum = 0;
+
+        while(n != 1)
+        {
+            if (list.Contains(n)) 
+                return false;
+
+            list.Add(n);
+            while (n > 0) 
+            {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return true;
+    }
+}
