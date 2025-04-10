4a7d0a6b6fb2bc09aadfdfa3315ab094319f3e4c
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:24:46 +0500

    Intersection of Two Arrays

---
+++ Problem#349/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution 
+{
+    public int[] Intersection(int[] nums1, int[] nums2) 
+    {
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        var result = new List<int>();
+
+        foreach (var num in nums1)
+        {
+            if (!list1.Contains(num))
+                list1.Add(num);
+        }
+        foreach (var num in nums2)
+        {
+            if (!list2.Contains(num)) 
+                list2.Add(num);
+        }
+        foreach (var num in list1)
+        {
+            if (list2.Contains(num))
+                result.Add(num);
+        }
+        return result.ToArray();
+    }
+}
