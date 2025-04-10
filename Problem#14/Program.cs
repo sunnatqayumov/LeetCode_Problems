c5e534918dd0c29f4f1ea96e5afeb6b8e15e6dac
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:03:34 +0500

    Longest Common Prefix

---
+++ Problem#14/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution 
+{
+    public string LongestCommonPrefix(string[] strs) 
+    {
+        if (strs == null)
+            return "";
+
+        Array.Sort(strs);
+
+        string harf = strs[0];
+
+        string s = strs[strs.Length - 1];
+
+        int i = 0;
+
+        while(i < harf.Length && i < s.Length && harf[i] == s[i])
+        {
+            i++;
+        }
+        return harf.Substring(0, i);
+    }
+}
