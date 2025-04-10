2e021d5dc3bf677bfe6a090f9527aff72c92b21e
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 09:22:39 +0500

    Move Zeroes

---
+++ Problem#283/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution 
+{
+    public void MoveZeroes(int[] nums) 
+    {
+        int left = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[left] = nums[i];
+                if(left != i)
+                {
+                    nums[i] = 0;
+                }
+                left++;
+            }
+        }
+    }
+}
