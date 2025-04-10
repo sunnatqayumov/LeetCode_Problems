943f58cc4e743b84704f3d71e0a10a6248b650dc
Sunnat <sunnatqayumov92@gmail.com>
2025-04-10 08:44:07 +0500

    Longest Substring Without Repeating Characters

---
+++ Problem#3/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string str)
+    {
+        string Substring = "";
+        int maxLength = 0;
+
+        foreach(char c in str)
+        {
+            if(Substring.Contains(c))
+            {
+                int index = Substring.IndexOf(c);
+                Substring = Substring.Substring(index + 1);
+            }
+            Substring += c;
+
+            maxLength = Math.Max(maxLength, Substring.Length);
+        }
+        return maxLength;
+    }
+}
