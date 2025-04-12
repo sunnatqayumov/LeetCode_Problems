5c847f872ad1bef00577ae51f64deeab1674a2c6
Sunnat <sunnatqayumov92@gmail.com>
2025-04-12 17:38:45 +0500

    Missing Number

---
+++ Problem#268/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution 
+{
+    public int MissingNumber(int[] nums) 
+    {
+        int result = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            result += (i + 1) - nums[i];
+        }
+        return result;
+    }
+}
