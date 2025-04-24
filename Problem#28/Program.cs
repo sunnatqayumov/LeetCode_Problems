21c87a0499f8f5a33e7fd058c57ef659e12af265
Sunnat <sunnatqayumov92@gmail.com>
2025-04-24 08:34:30 +0500

    Find the Index of the First Occurrence in a String

---
+++ Problem#28/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle) 
+    {
+        if (needle.Length == 0)
+        {
+            return 0;
+        }
+        if (haystack.Length < needle.Length)
+        {
+            return -1;
+        }
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
