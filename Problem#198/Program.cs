cdbf9fa49e345f112e881f5ddfd191f12cc0b768
Sunnat <sunnatqayumov92@gmail.com>
2025-05-05 19:57:38 +0500

    House Robber

---
+++ Problem#198/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        int a = 0, b = 0;
+
+        if(nums.Length == 0)
+            return 0;
+
+        if(nums.Length == 1)
+            return nums[0];
+
+        foreach(int num in nums)
+        {
+            int temp = a;
+            a = Math.Max(b + num, a);
+            b = temp;
+        }
+        return a;
+    }
+}
