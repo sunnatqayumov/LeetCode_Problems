14f1419ad82c951cdbc751b46111396d44ce93b6
Sunnat <sunnatqayumov92@gmail.com>
2025-05-07 19:35:55 +0500

    Multiply strings

---
+++ Problem#43/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution 
+{
+    public string Multiply(string num1, string num2) 
+    {
+        BigInteger son1 = BigInteger.Parse(num1);
+        BigInteger son2 = BigInteger.Parse(num2);
+
+        BigInteger son3 = son1 * son2;
+
+        return son3.ToString();
+    }
+}
