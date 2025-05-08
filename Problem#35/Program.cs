d9bb5206d153fe59e3969595c04bc797e561277b
Sunnat <sunnatqayumov92@gmail.com>
2025-05-08 12:48:59 +0500

    Search Insert Position

--- Problem#35/Program.cs
+++ Problem#35/Program.cs
@@ -2,26 +2,13 @@
 {
     public int SearchInsert(int[] nums, int target) 
     {
-        int left = 0;
-        int right = nums.Length - 1;
-
-        while (left <= right) 
+        for (int i = 0; i < nums.Length; i++) 
         {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) 
+            if (nums[i] >= target) 
             {
-                return mid;
-            }
-            else if (nums[mid] < target) 
-            {
-                left = mid + 1;
-            } 
-            else 
-            {
-                right = mid - 1;
+                return i;
             }
         }
-        return left;
+        return nums.Length;
     }
 }
