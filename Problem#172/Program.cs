287f295c4099be6729f48e9c059e7c2ec72f3f0e
Sunnat <sunnatqayumov92@gmail.com>
2025-05-08 16:38:30 +0500

    Problem#172

---
+++ Problem#172/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution 
+{
+    public int TrailingZeroes(int n) 
+    {
+        var count = 0;
+
+        while (n > 0)
+        {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
