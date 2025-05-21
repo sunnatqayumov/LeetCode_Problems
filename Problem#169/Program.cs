238d480b348dc8cd739fa64ab7d81cd6510bac39
Sunnat <sunnatqayumov92@gmail.com>
2025-05-21 19:57:00 +0500

    Majority Element

---
+++ Problem#169/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution 
+{
+    public int MajorityElement(int[] nums) 
+    {
+        int count = 0;
+        int son = 0;
+
+        foreach (int num in nums) 
+        {
+            if (count == 0) 
+            {
+                son = num;
+            }
+            count += (num == son) ? 1 : -1;
+        }
+        return son;
+    }
+}
