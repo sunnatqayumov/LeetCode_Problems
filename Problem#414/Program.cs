4c28b412fcaf592dcc1a388123f8278315b70aa7
Sunnat <sunnatqayumov92@gmail.com>
2025-05-21 20:00:31 +0500

    Third Maximum Number

---
+++ Problem#414/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution 
+{
+    public int ThirdMax(int[] nums) 
+    {
+        var result = nums.Distinct().OrderByDescending(n => n).ToList();
+
+        if(result.Count < 3)
+        {
+            return result[0];
+        }
+        return result[2];
+    }
+}
