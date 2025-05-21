59e8d3a30feed3cdf7a216617d062e72ba8168de
Sunnat <sunnatqayumov92@gmail.com>
2025-05-21 19:58:26 +0500

    Contains Duplicate

---
+++ Problem#217/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        List<int> result = [];
+
+        foreach(var num in nums)
+        {
+            if(result.Contains(num))
+                return true;
+
+            result.Add(num);
+        }
+        return false;
+    }
+}
