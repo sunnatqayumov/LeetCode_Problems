6bf36de4030ecebbd46c752ab332599771d66e8a
Sunnat <sunnatqayumov92@gmail.com>
2025-05-23 19:16:54 +0500

    Find the Difference

---
+++ Problem#389/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+
+        var sList = s.ToCharArray();
+        var tList = t.ToCharArray();
+
+        Array.Sort(sList);
+        Array.Sort(tList);
+
+        for (int i = 0; i < sList.Length; i++)
+        {
+            if (sList[i] != tList[i])
+                return tList[i];
+        }
+        return tList[tList.Length - 1];
+    }
+}
