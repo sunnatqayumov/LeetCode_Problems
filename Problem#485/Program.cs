044372a8c04d544c9b29905443ec68acb98951d0
Sunnat <sunnatqayumov92@gmail.com>
2025-05-23 18:09:27 +0500

    Max Consecutive Ones

---
+++ Problem#485/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution 
+{
+    public int FindMaxConsecutiveOnes(int[] nums) 
+    {
+        int count = 0, result = 0;
+
+        foreach (var num in nums)
+        {
+            if(num == 0)
+            {
+                result = Math.Max(result, count);
+                count = 0;
+            }
+            else
+                count++;
+        }
+        return Math.Max(result, count);
+    }
+}
