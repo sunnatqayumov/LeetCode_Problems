e23c46450b43d8e1a4845948a78e4e27595fdb1e
Sunnat <sunnatqayumov92@gmail.com>
2025-05-23 19:15:06 +0500

    Valid anagram

---
+++ Problem#242/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        var sList = s.ToCharArray();
+        var tList = t.ToCharArray();
+
+        Array.Sort(sList);
+        Array.Sort(tList);
+
+        for (int i = 0; i < sList.Length; i++)
+        {
+            if (sList[i] != tList[i])
+                return false;
+        }
+        return true;
+    }
+}
