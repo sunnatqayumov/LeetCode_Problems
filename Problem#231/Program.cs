784ab1c4c700f6a2118d3435a31c87b04dfbce51
Sunnat <sunnatqayumov92@gmail.com>
2025-05-27 19:29:05 +0500

    Power of two

---
+++ Problem#231/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+
+        while (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        if (n == 1)
+            return true;
+        else
+            return false;
+    }
+}
