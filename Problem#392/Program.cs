f250141b06de3f48db94f147bfd2c2279097277c
Sunnat <sunnatqayumov92@gmail.com>
2025-05-27 21:42:25 +0500

    Is Subsequence

---
+++ Problem#392/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var tList = t.ToCharArray().ToList();
+        var sList = s.ToCharArray();
+
+        var result = 0;
+
+        for (int i = 0; i < tList.Count; i++)
+        {
+            if(result < sList.Length && tList[i] == sList[result])
+            {
+                result++;
+            }
+        }
+        if(result == sList.Length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
