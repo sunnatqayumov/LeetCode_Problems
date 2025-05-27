77c7236b1e2cbb655ced39dc59a643fb698d8660
Sunnat <sunnatqayumov92@gmail.com>
2025-05-27 19:30:06 +0500

    Reverse string

---
+++ Problem#344/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public void ReverseString(char[] s) 
+    {
+        char[] reversed = s.Reverse().ToArray();
+
+        for (int i = 0; i < s.Length; i++) 
+        {
+            s[i] = reversed[i];
+        }
+    }
+}
