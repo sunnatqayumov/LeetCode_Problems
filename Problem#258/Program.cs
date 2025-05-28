ac38dbf9ccc4f32de9253cb43a97b8363b30bee4
Sunnat <sunnatqayumov92@gmail.com>
2025-05-28 20:10:01 +0500

    Add Digits

---
+++ Problem#258/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution 
+{
+    public int AddDigits(int num) 
+    {
+        for(int i = 0; i < num; i++)
+        {
+            int a = 0;
+            int b = 0;
+
+            if(num >= 1 && num < 10)
+            {
+                return num;
+            }
+            else
+            {
+                a = num / 10;
+                b = num % 10;
+                num = a + b;
+            }
+        }
+        return num;
+    }
+}
